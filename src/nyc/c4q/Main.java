ac1a9afd52037ee01a0be1d23cfacad495b06a90
Josiel <jdlopez114@gmail.com>
2016-08-31 18:05:46 -0400

    Pre work 8/30

--- src/nyc/c4q/Main.java
+++ src/nyc/c4q/Main.java
@@ -1,24 +1,57 @@
 package nyc.c4q;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         Hangman hangman = new Hangman();
-        while (hangman.getMisses() < 5){
-            hangman.printCurrentWord();
-            hangman.promptPlayer("Enter a letter: ");
-            hangman.readLetter();
-            hangman.checkLetter();
-            if(hangman.guessedSuccessfully()){
-                break;
+        boolean notDone = true;
+        while (notDone) {
+            while (hangman.getMisses() < 5) {
+                hangman.printCurrentWord();
+                hangman.promptPlayer("Enter a letter: ");
+                hangman.readLetter();
+                hangman.checkLetter();
+                if (hangman.guessedSuccessfully()) {
+                    break;
+                }
+                System.out.println(Drawing.get(hangman.getMisses()));
+                System.out.println("Misses -> " + hangman.getMisses());
             }
-            System.out.println(Drawing.get(hangman.getMisses()));
-            System.out.println("Misses -> " +hangman.getMisses());
+            if (hangman.guessedSuccessfully()) {
+                System.out.println("Success");
+            } else {
+                System.out.println("The answer was " + hangman.getSecretWord());
+            }
+            System.out.println("Do you want to play again?");
+
+        public static String readString () {
+            Scanner scanner = new Scanner(System.in);
+            return scanner.next();
         }
-        if(hangman.guessedSuccessfully()){
-            System.out.println("Success");
-        } else {
-            System.out.println("The answer was " + hangman.getSecretWord());
+
+
+        boolean isValidInput = true;
+
+        do {
+            String userAnswer = "getInput";
+            userAnswer = userAnswer.toLowerCase();
+            if (userAnswer.equals("yes") || userAnswer.equals("y")) {
+                notDone = true;
+            } else if (userAnswer.equals("no") || userAnswer.equals("n")) {
+                notDone = false;
+            } else {
+                System.out.println("Please select Yes or No.");
+            }
+
+        } while (!isValidInput){
+
+            if (isValidInput == true) {
+
+            }
+
         }
+    }
     }
 }
